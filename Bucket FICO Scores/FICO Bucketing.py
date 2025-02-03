ae0f24be81b7695b40d8b9ea401e5daa099d502c
C00lplayer <bibidhkafle@icloud.com>
2025-02-03 15:58:21 +1100

    Added Single Runable python File

---
+++ Bucket FICO Scores/FICO Bucketing.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data= pd.read_csv("Task 3 and 4_Loan_Data.csv")
+
+
+def find_squared_error(fico_bucket):
+    ni= len(fico_bucket)
+    ki = fico_bucket["default"].sum()
+    mse = ((fico_bucket["default"]-(ki/ni))**2).sum()
+    return mse
+
+
+
+def find_mse(infodf):
+    total_sum_mse = 0
+    for bucket in range(infodf["buckets"].max()+1):
+        bucket_data = infodf[infodf["buckets"]==bucket]
+        total_sum_mse+= find_squared_error(bucket_data)
+    return total_sum_mse/len(infodf)
+
+
+
+def optimise_buckets(data_df):
+    mean_mse_vals= []
+    for i in range(2,15):
+        data_df["buckets"]= pd.qcut(data_df["fico_score"], q = i, labels = False).astype(int)
+        mean_mse = find_mse(data_df)
+        mean_mse_vals.append((mean_mse,i))
+    min_mean_mse = min(mean_mse_vals, key= lambda x:x[0])
+    """
+    bucket_counts, mse_values = zip(*mean_mse_vals)
+    plt.plot(mse_values, bucket_counts, marker='o')
+    plt.xlabel("Number of Buckets")
+    plt.ylabel("Mean Squared Error")
+    plt.title("Number of Buckets vs. MSE")
+    plt.show()
+    """
+    print(f"Optimal Buckets = {min_mean_mse[1]} and MSE = {min_mean_mse[0]}")
+    return
+    
+
+
+
+optimise_buckets(data)
+    
